b14a8d86e1c4a8484a9a12a75dcc5edac460ec22
dlsgp <khyioh4413@gmail.com>
2024-03-27 13:23:26 +0900

    240327 빈 조회 Role ROLE_APPLICATION (애플리케이션에 등록된 빈) Role ROLE_INFRASTRUCTURE(스프링 내부에서 사용하는 빈) md작성

--- src/main/java/hello/springcore/AppConfig.java
+++ src/main/java/hello/springcore/AppConfig.java
@@ -9,17 +9,24 @@ import hello.springcore.member.MemberServiceImpl;
 import hello.springcore.member.MemoryMemberRepository;
 import hello.springcore.order.OrderService;
 import hello.springcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();   // 할인정책을 변경
         return new RateDiscountPolicy();
@@ -29,11 +36,6 @@ public class AppConfig {
 
 }
 /*
-애플리케이션의 전체 동작 방식을 구성(Config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스를 만들자.
-AppConfig는 생성한 객체 인스턴의 참조(레프런스)를 생성자를 통해서 주입(연결) 해준다.
- - 각 크르래스에 생성자가 없어 컴파일 오류 발생하므로 생성자를 만든다.
- - MemberServiceImpl은 이제부터 의존관계에 대한  고민은 외부(AppConfig)에 맡기고 실행에만 집중하면 된다.
- - Config 객체는 memory MemberRepository 객체를 생성하고 그 참조값을 memberServiceImpl을 생성하면서생성자로 전달된다.
- - 클라이언인 memberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다하여 DI(DEpendency Injection) 의존관계 주입 또는 의존성 주입이라 합니다.
- - AppConfig를 보면 역할(인터페이스)과 구현 클래스가 한눈에 들어온다. 그래서 앱 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있다.
+AppConfig에 설정을 구성한다는 뜻의 @Configuration
+각 메서드에 @Bean 을 붙여주면 스프링 컨테이너에 스프링 빈으로 등록한다.
  */
--- src/main/java/hello/springcore/OrderApp.java
+++ src/main/java/hello/springcore/OrderApp.java
@@ -5,11 +5,17 @@ import hello.springcore.member.*;
 import hello.springcore.order.Order;
 import hello.springcore.order.OrderService;
 import hello.springcore.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
-        OrderService orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//        MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
+//        OrderService orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService= memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
